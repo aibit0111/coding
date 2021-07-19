ba80021b70c4a87535c54ddaa34729847ed281fb
Light <vegetasama0.88@gmail.com>
2021-07-19 18:57:10 +0530

    Time: 4 ms (61.25%), Space: 7.2 MB (29.67%) - LeetHub

---
+++ rotate-image/rotate-image.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        
+        int row = matrix.size();
+        int col = matrix[0].size();
+        int temp;
+        for(int i = 0; i<row; i++){
+            for(int j = 0; j<i ; j++){
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        
+        for(int i = 0; i<col/2; i++){
+            for(int j = 0; j<row; j++){
+                temp = matrix[j][i];
+                matrix[j][i] = matrix[j][col-i-1];
+                matrix[j][col-i-1] = temp;
+            }
+        }
+        
+        
+        
+        
+    }
+};
