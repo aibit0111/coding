62b0940f4017f72cb665ff0a13b94654292cbddf
Light <vegetasama0.88@gmail.com>
2021-08-10 14:09:00 +0530

    Time: 4 ms (60.69%), Space: 7.1 MB (70.99%) - LeetHub

--- rotate-image/rotate-image.cpp
+++ rotate-image/rotate-image.cpp
@@ -5,23 +5,22 @@ public:
         int row = matrix.size();
         int col = matrix[0].size();
         int temp;
-        for(int i = 0; i<row; i++){
-            for(int j = 0; j<i ; j++){
+        
+        for(int i =0; i<row; i++){
+            for(int j =0; j<=i; j++){
                 temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
             }
         }
         
-        for(int i = 0; i<col/2; i++){
-            for(int j = 0; j<row; j++){
+        for(int i =0; i<col/2; i++){
+            for(int j =0; j<row; j++){
                 temp = matrix[j][i];
-                matrix[j][i] = matrix[j][col-i-1];
-                matrix[j][col-i-1] = temp;
+                matrix[j][i] = matrix[j][col-1-i];
+                matrix[j][col-1-i] = temp; 
             }
         }
-        
-        
         
         
     }
