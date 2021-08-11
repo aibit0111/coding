6222fbd019f772332898e781abc95ce9095a550a
Light <vegetasama0.88@gmail.com>
2021-08-11 08:31:48 +0530

    Time: 88 ms (8.06%), Space: 8.5 MB (91.61%) - LeetHub

--- find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -1,71 +1,54 @@
 class Solution {
 public:
     
-    int helper(unordered_map<char, int> umap){
-        int count  = 0;
-        for(auto i : umap){
-            if(i.second != 0) {
+    int helper(int array[26]){    
+        for(int i =0; i<26; i++){
+            if(array[i] != 0){
                 return 0;
             }
         }
-      return 1;
+        return 1;
     }
+    
     
     vector<int> findAnagrams(string s, string p) {
         
+        int length2 = p.length();
+        int array[26] = {0};
+        int idx;
         vector<int> result;
         
-        if(s.length() < p.length()) return result;
+        if(p.length()>s.length()) return result;
         
-        unordered_map<char, int> umap;
-        int length2 = p.length();
-        for (int i = 0; i<p.size(); i++){
-            umap[p[i]]++;
+        for(int i= 0; i<p.length(); i++){
+            idx = p[i] - 'a';
+            array[idx]++;    
         }
         
-        int i = 0;
-        int j = 0;
-        char c;
-        for(int k = j; k<length2; k++){
-            c = s[k];
-            auto it = umap.find(c);
-            if(it != umap.end()){
-                (it->second)--;
-            }
+        for(int i =0; i<length2; i++){
+            idx = s[i] - 'a';
+            array[idx]--;
         }
         
-        int check;
-        check = helper(umap);
-        if(check == 1){
-            result.push_back(i);
-        }
-        char c1;
-        char c2;
-        j = length2-1;
-        while(j!=s.length()-1){
-            cout<<i<<" "<<j;
-            c1 = s[i];
-            auto it = umap.find(c1);
-            if(it!= umap.end()){
-                umap[c1]++;
-            }
-            i++;
-            j++;
-            c2 = s[j];
-            auto itt = umap.find(c2);
-            if(itt!= umap.end()){
-                (itt->second)--;
-            }
-
-            check = helper(umap);
-            cout<<check;
-            if(check == 1){
-                result.push_back(i);
-            }
+        if(helper(array)) result.push_back(0);
         
+        int left = 0;
+        int right = length2-1;
+        int pidx;
+        int nidx;
+        
+        while(right<s.length()){
+            cout<<left<<right;
+            pidx = s[left] - 'a';
+            array[pidx]++;
+            left++;
+            right++;
+            if(right<s.length()) nidx = s[right] - 'a';
+            cout<<"n"<<left<<" ";
+            array[nidx]--;
+            if(helper(array) && right <s.length()) result.push_back(left);
         }
         
         return result;
-        
     }
 };
