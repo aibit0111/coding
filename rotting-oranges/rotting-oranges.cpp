4615158bf644264d2ba2878fb7a046b98084fe47
Light <vegetasama0.88@gmail.com>
2021-08-15 15:43:52 +0530

    Time: 8 ms (58.40%), Space: 13.1 MB (51.76%) - LeetHub

--- rotting-oranges/rotting-oranges.cpp
+++ rotting-oranges/rotting-oranges.cpp
@@ -1,60 +1,60 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
+        int fresh=0;
         int row = grid.size();
         int col = grid[0].size();
-        queue<pair<int,int>> q;
-        int fresh = 0;
-        int time = 0;
+        queue<pair<int, int>> q;
         
-        for(int i = 0; i<row; i++){
-            for(int j =0; j<col; j++){
-                if(grid[i][j] == 2){
+        for(int i =0; i<grid.size(); i++){
+            for(int j =0; j <col; j++){
+                if( grid[i][j] == 2){
                     q.push({i,j});
                 }
-                if(grid[i][j] == 1) fresh++;
+                
+                if(grid[i][j] == 1){
+                    fresh++;
+                }
             }
         }
-        
+        int qsize ;
+        int time = 0;
+        int x,y;
         while(!q.empty()){
-            int qsize = q.size();
-            cout<<"hello";
-            for(int i = 0; i<qsize; i++){
-                int x = q.front().first;
-                int y = q.front().second;
+            qsize = q.size();
+            for(int i =0; i<qsize; i++){
+                x = q.front().first;
+                y = q.front().second;
                 q.pop();
-                
-                if(x>0 && grid[x-1][y] == 1){
+                if(x>0 && grid[x-1][y] == 1) {
                     grid[x-1][y] = 2;
                     q.push({x-1,y});
-                    fresh--;
+                    fresh--;    
                 }
-                
-                if(y>0 && grid[x][y-1] == 1){
-                    grid[x][y-1] = 2;
-                    q.push({x,y-1});
-                    fresh--;
-                }
-                
-                if(x<row-1 && grid[x+1][y] == 1){
+                if(x<row-1 && grid[x+1][y] == 1) {
                     grid[x+1][y] = 2;
-                    q.push({x+1,y});
+                    q.push({x+1, y});
                     fresh--;
                 }
-                
-                if(y<col-1 && grid[x][y+1] == 1){
-                    grid[x][y+1] = 2;
-                    q.push({x,y+1});
+                if(y>0 && grid[x][y-1] == 1) {
+                    grid[x][y-1] = 2;
+                    q.push({x , y-1});
                     fresh--;
                 }
+                if(y<col-1 && grid[x][y+1] == 1) {
+                    grid[x][y+1] =2;
+                    q.push({x , y+1});
+                    fresh--;
+                }
+            
             }
-            if(!q.empty()) time++;
+            if(q.empty() == false) time++;
+            
         }
         
-        if(fresh == 0){
-            return time;
-        }
-        return -1;
+        if(fresh > 0) return -1;
+        return time;
+        
         
     }
 };
